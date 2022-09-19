dcdaf9d8aef279b22da108a083e15a52afcccd7b
sleonelo <olmoscarrizosilvioleonel@gmail.com>
2022-09-19 18:25:15 -0300

    19/09

--- ej6.py
+++ ej6.py
@@ -4,19 +4,47 @@
  En caso de ser festivo, la tarifa se incrementa en un 10% en caso de turno diurno y en un 15% para el nocturno.
 Desarrolle una función que permita ingresar por teclado la siguiente información para, al menos, 2 empleados, nombre del trabajador, el número de horas trabajadas, el turno y el tipo de día (“Festivo”, “Laborable”), para ello se podría utilizar 1 “diccionario” para registrar la información y si los datos ingresados son correctos llamar a otra función que realice el cálculo del sueldo a cobrar en ese día. Mostrar por pantalla los datos ingresados y el sueldo calculado para cada empleado."""
 
+
+
 trabajador={}
-datos=[]
+dato=[]
+dato1=[]
+while True:
+    nombre=input("ingrese nombre: ")
+    horas=int(input("ingrese horas: "))
+    turno=input("ingrese turno: ")
+    dia=input("ingrese dia: ")
 
-nombre=input("ingrese nombre: ")
-horas=int(input("ingrese horas: "))
-turno=input("ingrese turno: ")
-dia=input("ingrese dia: ")
-
-def datos(nombre, horas, turno, dia):
-    trabajador[nombre]=[horas,turno,dia]
-    return(trabajador)
-
-
+    def datos(nombre, horas, turno, dia):
+        trabajador[nombre]=[horas,turno,dia]
+        dato.append(trabajador)
+        for i in range(len(dato)):
+            if len(dato)>1:
+                dato.pop()
+        
+        return(dato)    
+        
+    def calculo(dato):  
+        for i in dato:    
+            lista=list(i.values())
+            trabajador=list(i.keys())
+            for i in range(len(lista)):
+                if "nocturno" in lista[i]:
+                    if "festivo" in lista[i]:
+                        print(trabajador[i], 400*1.15*lista[i][0])
+                    else:
+                        print(trabajador[i], 400*lista[i][0])
+                else:
+                    if "festivo" in lista[i]:
+                        print(trabajador[i],350*1.10*lista[i][0])
+                    else:
+                        print(trabajador[i],350*lista[i][0])  
+            
+                
+      
+    datos(nombre, horas, turno, dia)
+    calculo(dato)  
+    continue
     
 
 
---
+++ prueba.py
@@ -0,0 +1,69 @@
+
+"""def funcion_suma_sueldo ():
+    diccionario_sueldo_años = {
+    2019: 100000,
+    2020: 110000,
+    2021: 125000,
+    2022: 140000
+    }
+    suma_sueldo = 0
+    for sueldo in diccionario_sueldo_años.values():
+        suma_sueldo += sueldo
+    print ("SUMATORIA:  ", suma_sueldo)
+
+funcion_suma_sueldo()"""
+
+"""def funcion_suma_sueldo(parametro_diccionario):
+    suma_sueldo = 0
+    for sueldo in parametro_diccionario.values():
+        suma_sueldo += sueldo
+    return suma_sueldo
+
+diccionario_sueldo_años = {
+2019: 100000,
+2020: 110000,
+2021: 125000,
+2022: 140000
+}
+calculo_sumatoria = funcion_suma_sueldo(diccionario_sueldo_años)
+print ("Sumatoria de Sueldos devuelta por la Funcion: ", calculo_sumatoria)
+"""
+"""
+trabajador={}
+dato=[]
+dato1=[]
+while True:
+    nombre=input("ingrese nombre: ")
+    horas=int(input("ingrese horas: "))
+    turno=input("ingrese turno: ")
+    dia=input("ingrese dia: ")
+
+    def datos(nombre, horas, turno, dia):
+        dato.append(nombre)
+        dato.append(horas)
+        dato.append(turno)
+        dato.append(dia)
+                           
+        print(dato)    
+        
+    def calculo(**dato):
+        print(len(dato))
+    
+    
+        
+    datos(nombre, horas, turno, dia)
+    calculo(dato)  
+    continue
+"""
+def funcion_prueba(**52,36,69,15,7):
+    print("Diccionario completo: ",parametros)
+    for clave in parametros.keys():
+        print (" Clave: ", clave, end=",")
+    total = 0
+    for value in parametros.values():
+        total += value
+        return total
+    valor_retornado = funcion_prueba(a=10, b=20, c=30, d=40, e=50)
+    print ("\nValor devuelto por la Funcion (1): " , valor_retornado)
+    valor_retornado = funcion_prueba(a=10, b=20)
+    print ("\nValor devuelto por la Funcion (2): " , valor_retornado)
