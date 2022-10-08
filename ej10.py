9f02dbaf8f1fa95ce0dc1bf732edc7a39f2f0c3a
sleonelo <olmoscarrizosilvioleonel@gmail.com>
2022-10-08 18:12:02 -0300

    "8/10"

--- ej10.py
+++ ej10.py
@@ -1,8 +1,8 @@
 """10. Como ejercicio, escriba una función que use la funcion tomaNumero para leer un número del teclado y que maneje la excepción ErrorNumeroMalo."""
 
 class ErrorNumeroMalo(BaseException):
-    def __init__(self, message):
-        self.message = message
+    def __init__(self, mensaje):
+        self.message = mensaje
         
 def elige_numero():
     numero = input("Escriba un número: ")
---
+++ operaciones.py
@@ -0,0 +1,13 @@
+#se debe empezar en el renglon 3
+
+def suma(a,b):
+    return (a+b)
+
+def resta(a,b):
+    return(a-b)
+
+def producto(a,b):
+    return(a*b)
+
+def division(a,b):
+    return (a/b)
