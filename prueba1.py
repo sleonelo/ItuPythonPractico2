c947468cd15aacc1533eb9c1dba91e0adb903009
sleonelo <olmoscarrizosilvioleonel@gmail.com>
2022-10-10 16:31:36 -0300

    "10/10"

--- prueba1.py
+++ prueba1.py
@@ -2,3 +2,10 @@ cadena="la arañita de martita"
 
 if any(i=="a" for i in cadena):
     print("no hay una 'z' bro")
+    
+    
+var=input("ingrese A o B: ")
+var=var.upper()
+print(var)
+while var not in ("A","B"):
+    var=input("ingrese A o B: ")
